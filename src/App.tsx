81f417c8ff8a585aaba89767e521e779e329a03d
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2024-11-07 09:08:25 -0300

    feat: simples form with react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { useForm } from "react-hook-form";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+
 export default function App() {
+  const { handleSubmit: submit, register } = useForm<{
+    name: string;
+    age: number;
+  }>();
+
+  const handleSubmit = submit(async (data) => {
+    console.log(data);
+  });
+
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-xl font-bold text-zinc-800">Hello Word</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+        <Input type="text" placeholder="Nome" {...register("name")} />
+        <Input type="number" placeholder="Idade" {...register("age")} />
+
+        <Button type="submit">Enviar</Button>
+      </form>
     </div>
-  )
+  );
 }
